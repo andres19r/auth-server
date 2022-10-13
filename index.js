539a22fc0d121e6fa7eb1446ffbde497734af9c8
Andres Rivero <andres19rivero@gmail.com>
2022-10-12 20:32:39 -0400

    test the express server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello from node')
+const express = require("express")
+
+// Create the express server/app
+const app = express();
+
+// GET
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'OK',
+    uuid: 1234
+  })
+})
+
+app.listen(4000, () => {
+  console.log(`Server running on port ${4000}`)
+})
