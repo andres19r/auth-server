9bdf4b922f364823d2c32007bd2295f5fccda8c5
Andres Rivero <andres19rivero@gmail.com>
2022-10-14 23:44:08 -0400

    config cors & get requests body

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 const { response } = require('express')
 
 const createUser = (req, res = response) => {
+  const { email, name, password } = req.body;
+  console.log(email, name, password)
+
   return res.json({
     ok: true,
     msg: 'Create user /new'
@@ -8,6 +11,9 @@ const createUser = (req, res = response) => {
 }
 
 const loginUser = (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password)
+
   return res.json({
     ok: true,
     msg: 'User login /'
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 
 // Create the express server/app
 const app = express();
 
+// CORS
+app.use(cors());
+
+// Reading & body parsing
+app.use(express.json());
+
 // Routes
-app.use('/api/auth', require('./routes/auth'))
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(4000, () => {
-  console.log(`Server running on port ${4000}`)
-})
+  console.log(`Server running on port ${4000}`);
+});
