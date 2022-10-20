0fd47638b29ce795777af0418e9d5607824c0cba
Andres Rivero <andres19rivero@gmail.com>
2022-10-19 21:52:58 -0400

    create user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
-const { response } = require('express');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const { generateJWT } = require('../helpers/jwt')
+const { response } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { generateJWT } = require("../helpers/jwt");
 
 const createUser = async (req, res = response) => {
   const { email, name, password } = req.body;
@@ -12,7 +12,7 @@ const createUser = async (req, res = response) => {
     if (user) {
       return res.status(400).json({
         ok: false,
-        msg: 'The email is used by another user'
+        msg: "The email is used by another user",
       });
     }
 
@@ -34,36 +34,66 @@ const createUser = async (req, res = response) => {
       ok: true,
       uuid: dbUser.id,
       name,
-      token
-    })
-
-  } catch(error) {
+      token,
+    });
+  } catch (error) {
     console.log(error);
     return res.status(500).json({
       ok: false,
-      msg: 'Please, contact with your admin'
-    })
+      msg: "Please, contact with your admin",
+    });
   }
-}
+};
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
 
-  return res.json({
-    ok: true,
-    msg: 'User login /'
-  })
-}
+  try {
+    const dbUser = await User.findOne({ email });
+    if (!dbUser) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The email doesn't exist",
+      });
+    }
+
+    // Confirm password match
+    const validPassword = bcrypt.compareSync(password, dbUser.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The password is not valid",
+      });
+    }
+
+    // Generate JWT
+    const token = await generateJWT(dbUser.id, dbUser.name);
+
+    // Response
+    return res.json({
+      ok: true,
+      uuid: dbUser.id,
+      name: dbUser.name,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Please, contact with your admin",
+    });
+  }
+};
 
 const revalidateToken = (req, res) => {
   return res.json({
     ok: true,
-    msg: 'Renew'
-  })
-}
+    msg: "Renew",
+  });
+};
 
 module.exports = {
   createUser,
   loginUser,
-  revalidateToken
-}
+  revalidateToken,
+};
